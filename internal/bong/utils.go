410cfa01af58f5c742056bd98f392eeaae4072d6
npmania <np@mkv.li>
2022-12-02 01:26:02 +0900

    forgot removing debug println

--- internal/bong/utils.go
+++ internal/bong/utils.go
@@ -5,9 +5,6 @@ import "fmt"
 // TODO: hey, you should switch to hash map anyways.
 func FindBong(bongs []Bong, bongus string) Bong {
 	for _, b := range bongs {
-		if b.Bongus == "opensusesoftware" {
-			fmt.Println(b.Bongus, bongus)
-		}
 		if b.Bongus == bongus {
 			fmt.Println("mateched")
 			return b
