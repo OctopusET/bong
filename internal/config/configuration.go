94fd512f2cfd2fb7a431674cf190859f6a17ab77
npmania <np@mkv.li>
2022-12-02 03:57:01 +0900

    let config.yaml not tracked by git

--- internal/config/configuration.go
+++ internal/config/configuration.go
@@ -1,11 +1,13 @@
 package config
 
 import (
+	"errors"
 	"os"
 
 	"gopkg.in/yaml.v3"
 )
 
+const exampleFilename = "config.example.yaml"
 const filename = "config.yaml"
 
 type Config struct {
@@ -15,8 +17,27 @@ type Config struct {
 	DefaultPrefix string `yaml:"Default Prefix"`
 }
 
+func CopyIfNotExists() error {
+	if _, err := os.Stat(filename); errors.Is(err, os.ErrNotExist) {
+		data, err := os.ReadFile(exampleFilename)
+		if err != nil {
+			return err
+		}
+
+		if err = os.WriteFile(filename, data, 0600); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
 func LoadConfig() (Config, error) {
 	c := Config{}
+
+	if err := CopyIfNotExists(); err != nil {
+		return Config{}, err
+	}
 
 	raw, err := os.ReadFile(filename)
 	if err != nil {
