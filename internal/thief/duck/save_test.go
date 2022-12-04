57a28789096c36838e92adcdb8278fd0026f2685
npmania <np@mkv.li>
2022-12-05 00:57:42 +0900

    better test script for duckmeat

--- internal/thief/duck/save_test.go
+++ internal/thief/duck/save_test.go
@@ -1,12 +1,14 @@
-package duck
+package duck_test
 
 import (
 	"os"
 	"testing"
+
+	"github.com/npmania/bong/internal/thief/duck"
 )
 
 func TestUpdateBangs(t *testing.T) {
-	if err := UpdateBangs(); err != nil {
+	if err := duck.UpdateBangs(); err != nil {
 		panic(err)
 	}
 	os.RemoveAll("bongs")
